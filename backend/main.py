dc53f8948b818c2ecb0c26bc0bea55159f86c2d3
mominalvi <mominalvi30@gmail.com>
2024-10-17 16:50:26 -0400

    Integrate mp3 file receiving functionality

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,27 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import shutil
+from pathlib import Path
 
 app = FastAPI()
 
+@app.post("/upload-mp3")
+async def create_upload_file(file: UploadFile):
+    try:
+        # Define the folder where the file will be saved
+        upload_dir = Path("uploads")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+        # Ensure that the parent directory exsits
+        upload_dir.mkdir(parents=True, exist_ok=True)
+
+        # Construct the full path for the file
+        file_path = upload_dir / file.filename
+
+        # Open the file and save it
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        return {"info": f"file '{file.filename}' saved at '{file_path}'"}
+    
+    # If there is an error, return an error message
+    except Exception as e:
+        return {"message": str(e)}
